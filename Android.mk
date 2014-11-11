c43e3a2aa62dacc88670d7db23594e43cc68cd60
Ethan Chen <intervigil@gmail.com>
2014-11-10 19:06:23 -0800

    audio: Use project pathmap
    Change-Id: I8e3664e126048d75c3842e3abc6796d92873320d

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-audio)/legacy)
+
 ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -6,6 +8,8 @@ ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
 include $(MY_LOCAL_PATH)/legacy/Android.mk
 else
 include $(MY_LOCAL_PATH)/hal/Android.mk
+endif
+
 endif
 
 endif
